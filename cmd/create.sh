dfd3ad19ae9c56c8e25dc9f17825bd0d7464d9de
dashohoxha <dashohoxha@gmail.com>
2018-07-28 05:59:58 +0200

    Use the display of the host.

--- cmd/create.sh
+++ cmd/create.sh
@@ -8,9 +8,18 @@ _EOF
 
 rename_function cmd_create orig_cmd_create
 cmd_create() {
-    Xephyr :$XDISPLAY -screen 800x600 -resizeable -retro -extension MIT-SHM -extension XTEST &
-    sleep 2
+    # based on the instructions found here:
+    # https://stackoverflow.com/questions/16296753/can-you-run-gui-applications-in-a-docker-container
+
+    XSOCK=/tmp/.X11-unix
+    XAUTH=$(pwd)/.docker.xauth
+    xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+    chmod 644 $XAUTH
+
     orig_cmd_create \
-        --env DISPLAY=:$XDISPLAY \
-        --mount type=bind,src=/tmp/.X11-unix/X$XDISPLAY,dst=/tmp/.X11-unix/X$XDISPLAY
+        --net=host \
+        --mount type=bind,src=$XSOCK,dst=$XSOCK,readonly \
+        --mount type=bind,src=$XAUTH,dst=$XAUTH \
+        --env DISPLAY=$DISPLAY \
+        --env XAUTHORITY=$XAUTH
 }
